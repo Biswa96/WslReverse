7db58edc4082113f1a27e3518031a54972473a34
Biswapriyo Nath <nathbappai@gmail.com>
2020-09-28 19:19:45 +0530

    wslcli: Directly invoke IWslClient through DllGetClassObject.

--- wslcli/WslClient.c
+++ wslcli/WslClient.c
@@ -9,26 +9,50 @@
 
 int main(void)
 {
-    HRESULT hRes = 0;
+    HRESULT hRes;
+    DWORD dwRes;
+    HKEY hKey;
+    DWORD Type = REG_SZ, cbData = MAX_PATH;
+    char Data[MAX_PATH];
+
+    HMODULE hMod;
+    typedef HRESULT(WINAPI* DLLGETCLASSOBJECT)(const GUID* const rclsid, const GUID* const riid, void** ppv);
+    DLLGETCLASSOBJECT pfnDllGetClassObject;
+    IClassFactory* classFactory;
+    IWslClient* wslClient;
 
     hRes = CoInitializeEx(NULL, COINIT_MULTITHREADED);
     hRes = CoInitializeSecurity(NULL, -1, NULL, NULL,
                                 RPC_C_AUTHN_LEVEL_DEFAULT,
                                 SecurityDelegation, NULL,
                                 EOAC_STATIC_CLOAKING, NULL);
-
-    IWslClient* wslClient = NULL;
+#if 0
     hRes = CoCreateInstance(&CLSID_WslClient,
                             NULL,
                             CLSCTX_INPROC_SERVER,
                             &IID_IWslClient,
                             (PVOID*)&wslClient);
-    if (hRes)
-        return 1;
+#endif
 
-    DWORD Result;
-    hRes = wslClient->lpVtbl->Main(wslClient,
-                        WSL_CLENT_ENTRY_WSL, L"", &Result);
+    dwRes = RegOpenKeyExA(
+        HKEY_CLASSES_ROOT,
+        "CLSID\\{615A13BE-241D-48B1-89B0-8E1D40FFD287}\\InprocServer32",
+        0, KEY_READ, &hKey);
 
+    dwRes = RegQueryValueExA(hKey, NULL, NULL, &Type, (PBYTE)Data, &cbData);
+    dwRes = RegCloseKey(hKey);
+
+    hMod = LoadLibraryExA(Data, NULL, 0);
+
+    pfnDllGetClassObject = (DLLGETCLASSOBJECT)GetProcAddress(hMod, "DllGetClassObject");
+
+    hRes = pfnDllGetClassObject(&CLSID_WslClient, &IID_IClassFactory, &classFactory);
+
+    hRes = classFactory->lpVtbl->CreateInstance(classFactory, NULL, &IID_IWslClient, &wslClient);
+
+    hRes = wslClient->lpVtbl->Main(wslClient, WSL_CLENT_ENTRY_WSL, L"", &dwRes);
+
+    FreeLibrary(hMod);
+    CoUninitialize();
     return 0;
 }
--- wslcli/WslClient.h
+++ wslcli/WslClient.h
@@ -7,12 +7,14 @@
 #ifndef WSLCLIENT_H
 #define WSLCLIENT_H
 
+ /* {615A13BE-241D-48B1-89B0-8E1D40FFD287} */
 static const GUID CLSID_WslClient = {
     0x615A13BE,
     0x241D,
     0x48B1,
     { 0x89, 0xB0, 0x8E, 0x1D, 0x40, 0xFF, 0xD2, 0x87 } };
 
+/* {91A11D73-FC34-455C-861F-0A371E4B56AE} */
 static const GUID IID_IWslClient = {
     0x91A11D73,
     0xFC34,
@@ -24,7 +26,7 @@ typedef enum _WslClientEntry
     WSL_CLENT_ENTRY_BASH = 0,
     WSL_CLENT_ENTRY_WSL = 1,
     WSL_CLENT_ENTRY_WSLCONFIG = 2,
-    WSL_CLENT_ENTRY_WSLHOST = 3
+    /* WSL_CLENT_ENTRY_WSLHOST = 3, Removed from Win Build 20201 */
 } WslClientEntry;
 
 typedef struct _IWslClient IWslClient;
@@ -46,7 +48,7 @@ struct _IWslClientVtbl
         /* In */ IWslClient* This,
         /* In */ WslClientEntry EntryType,
         /* In */ PCWSTR CommandLine,
-        /* In */ PDWORD Result);
+        /* Out */ PDWORD Result);
 };
 
 struct _IWslClient
